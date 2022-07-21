92fca1464f3aa93f2a358347d640ceef12611903
Verry R. Wibawa <verryrwibawa@students.amikom.ac.id>
2022-07-21 13:20:54 +0700

    add Jawa & Sulawesi

--- Papua/script.js
+++ Papua/script.js
@@ -1,8 +1,71 @@
-$(".backToTop").click(function () {
-    window.scrollTo(0, 0);
-});
+window.addEventListener("load", function () {
+  $("#preloader").css("display", "none");
+  $("body").removeClass("scrolled");
 
-$(window).scroll(function () {
-    $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
-    $(".backToTop").toggleClass("show", $(this).scrollTop() > 100);
+  // --- Animate On Scroll ---
+  AOS.init({
+    duration: 700,
+    once: true,
+  });
+
+  // --- Dark Mode ---
+  let darkMode = localStorage.getItem("darkMode");
+
+  const enableDarkMode = () => {
+    $("body").addClass("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+  };
+
+  const disableDarkMode = () => {
+    $("body").removeClass("dark-mode");
+    localStorage.setItem("darkMode", null);
+  };
+
+  if (darkMode === "enabled") {
+    enableDarkMode();
+    $("#checkbox").prop("checked", true);
+  }
+
+  // dark mode checked
+  $("#checkbox").change(function () {
+    darkMode = localStorage.getItem("darkMode");
+
+    if (darkMode !== "enabled") {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  });
+
+  // --- Change Navbar On Scroll ---
+  let lastY = 0;
+  let currentY = $(this).scrollTop();
+
+  if (currentY > 0) {
+    $("body").addClass("scrolled");
+  }
+
+  $(window).scroll(function (event) {
+    lastY = currentY;
+    currentY = $(this).scrollTop();
+    if (currentY > lastY) {
+      $("body").addClass("scrolled");
+    } else {
+      $("body").removeClass("scrolled");
+    }
+    lastY = currentY;
+    if (currentY > 100) {
+      $("body").addClass("scrolled-100");
+    } else {
+      $("body").removeClass("scrolled-100");
+    }
+  });
+
+  // Parallax
+  $(window).scroll(function () {
+    const parallax = $("#showcase");
+    let scrollPosition = window.scrollY;
+
+    parallax.css("background-position", "center " + scrollPosition * 0.5 + "px");
+  });
 });
